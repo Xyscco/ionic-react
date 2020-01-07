9e95d52e1fbbea8e6628117145f15876eb30126d
Francisco Márcio da Costa Miranda <francisco@syncode.local>
2020-01-07 15:44:12 -0300

    Final da primeira pagina

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonFab, IonFabButton, IonIcon } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonFab, IonFabButton, IonIcon, IonButton } from '@ionic/react';
+import React, { useState } from 'react';
 import { add } from 'ionicons/icons';
 import { RouteComponentProps } from 'react-router';
+import { Plugins, CameraResultType } from '@capacitor/core';
 
 const Home: React.FC<RouteComponentProps> = (props) => {
+  const { Camera } = Plugins;
+  const [ photo, setPhoto ] = useState();
+  const takePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+    setPhoto(image.webPath);
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -24,6 +35,8 @@ const Home: React.FC<RouteComponentProps> = (props) => {
             </IonBadge>
           </IonItem>
         </IonList>
+        <img src={photo} />
+        <IonButton onClick={takePhoto}></IonButton>
         <IonFab vertical="bottom" horizontal="end" slot="fixed">
           <IonFabButton onClick={() => props.history.push('/new')}>
             <IonIcon icon={add} />
